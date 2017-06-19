cdaac893aa1f2e32a3170de35807c1c922129252
Sergey Bobrovskikh <qwolajin@gmail.com>
2017-06-19 14:38:03 +0300

    map_navigation

--- vane-examples/public/js/main.js
+++ vane-examples/public/js/main.js
@@ -1,25 +1,48 @@
 $(document).ready(function() {
-    $('img').error(function(e, b, c) {
+    $('.tile-preview').error(function(e, b, c) {
+        $(this).closest('a').removeClass('spinner');
         $(this).attr("src", "public/img/error.png");
     });
 
+    $('.tile-preview').load(function() {
+      $(this).closest('a').removeClass('spinner');
+    });
+
     $("#date-flag").change(function() {
-		    if(this.checked) {
-		        $("#daterange-flag").prop('checked', false);
-		    }
-		});
+        if (this.checked) {
+            $("#daterange-flag").prop('checked', false);
+        }
+    });
 
     $("#daterange-flag").change(function() {
-		    if(this.checked) {
-		        $("#date-flag").prop('checked', false);
-		    }
-		});
+        if (this.checked) {
+            $("#date-flag").prop('checked', false);
+        }
+    });
+    // /8/127/92
+    var map = L.map('map').setView([44.77, -0.6], 8);
 
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    var tileSize = L.point(256, 256);
     var refreshAll = function() {
-        var baseArr = $('#base-url').val().split('?', 2);
-        var host = $('#base-url').val();
+        $('.tile-preview').each(function() {            
+            $(this).closest('a').addClass('spinner');
+        });
 
+        var zoom = map.getZoom();
+        var center = map.getCenter();
+        var pixelPoint = map.project(center, zoom).floor();
+        var coords = pixelPoint.unscaleBy(tileSize).floor();
+
+        var baseArr = $('#base-url').val().split('?', 2);
         var op = $('.op-list.active').data('op');
+
+        var host = baseArr[0].replace("/{z}/{x}/{y}", "/" + zoom + "/" + coords.x + "/" + coords.y);
+        var query = baseArr[1];
+        var baseURL = host + '?op=' + op + (query ? '&' + query : '');
 
         var whereArr = [];
 
@@ -39,16 +62,11 @@ $(document).ready(function() {
             whereArr.push('day<' + end.format('YYYY-MM-DD'));
         }
 
-        var baseURL = baseArr[0] + '?op=' + op;
-        if (baseArr[1]) {
-            baseURL += '&' + baseArr[1];
-        }
-
         if (whereArr.length) {
             baseURL += '&where=' + whereArr.join(',');
         }
 
-        $('img').each(function() {
+        $('.tile-preview').each(function() {
             var $img = $(this);
             var imgFrom = $img.data('from');
 
@@ -59,14 +77,14 @@ $(document).ready(function() {
     };
 
     $('#go-button').click(function(e) {
-        setTimeout(refreshAll,1);
+        setTimeout(refreshAll, 1);
     });
 
     $('.op-list').click(function(e) {
         console.log(e);
         $('.op-list').removeClass('active');
         $(this).addClass('active');
-        setTimeout(refreshAll,1);
+        setTimeout(refreshAll, 1);
     });
 
     $('input[name="daterange"]').daterangepicker({
@@ -77,7 +95,7 @@ $(document).ready(function() {
             endDate: moment().format('YYYY-MM-DD')
         },
         function(start, end, label) {
-            setTimeout(refreshAll,1);
+            setTimeout(refreshAll, 1);
         });
 
 
@@ -91,17 +109,20 @@ $(document).ready(function() {
             endDate: moment().format('YYYY-MM-DD')
         },
         function(start, end, label) {
-            setTimeout(refreshAll,1);
+            setTimeout(refreshAll, 1);
         });
 
-    $('#clouds').change(function(){
+    $('#clouds').change(function() {
         $('#clouds-value').text($(this).val());
-    	setTimeout(refreshAll,1);
+        setTimeout(refreshAll, 1);
     });
 
-    $('.flag').change(function(){
-        setTimeout(refreshAll,1);
+    $('.flag').change(function() {
+        setTimeout(refreshAll, 1);
     });
 
-    setTimeout(refreshAll,1);
+    map.on('moveend', function(e) {
+        setTimeout(refreshAll, 1);
+    });
+    setTimeout(refreshAll, 1);
 });
