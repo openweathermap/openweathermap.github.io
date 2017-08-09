ab477ccb4303c159bfedea57e5bffac1665729ae
Sergey Bobrovskikh <qwolajin@gmail.com>
2017-08-09 13:24:45 +0300

    base_url

--- map-viewer/public/js/main.js
+++ map-viewer/public/js/main.js
@@ -6,7 +6,9 @@ $(document).ready(function() {
     }).addTo(map);
 
 
-    var url = $('#base-url').val();
+
+    var url = prepareUrl();
+
     var layer = L.tileLayer(url, {
         attribution: '&copy; <a href="http://owm.io">VANE</a>'
     });
@@ -14,7 +16,7 @@ $(document).ready(function() {
     layer.addTo(map);
 
     var refresh = function() {
-        url = $('#base-url').val();
+        var url = prepareUrl();
         layer.setUrl(url);
     };
 
@@ -28,3 +30,16 @@ $(document).ready(function() {
         }
     })
 });
+
+function prepareUrl() {
+    var baseArr = $('#base-url').val().split('?', 2);
+    var url = baseArr[0] + '?';
+
+    if (baseArr[0].indexOf('sat.owm.io') + 1) {
+        url += 'appid=9de243494c0b295cca9337e1e96b00e2&';
+    }
+
+    url += baseArr[1];
+
+    return url;
+};
