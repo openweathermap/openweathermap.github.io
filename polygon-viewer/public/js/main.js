ed838ffefbc7ac6c00647ff3fa807e0021a947ab
Sergey Bobrovskikh <sergey.bobrovskikh@owm.io>
2020-06-15 19:34:42 +0300

    on click

--- polygon-viewer/public/js/main.js
+++ polygon-viewer/public/js/main.js
@@ -9,6 +9,16 @@ $(document).ready(function() {
 		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
 
+	const getCoords = (map, ll) => {
+		var tileSize = L.point(256, 256);
+
+		var zoom = map.getZoom();
+		var pixelPoint = map.project(ll, zoom).floor();
+		var coords = pixelPoint.unscaleBy(tileSize).floor();
+
+		return "/" + zoom + "/" + coords.x + "/" + coords.y;
+	};
+
 	const decodeHash = (str) => {
 		const type = [
 				'l8',
@@ -65,11 +75,22 @@ $(document).ready(function() {
 				} else return;
 
 				const day = `${date.getUTCFullYear()}-${('0' + (date.getUTCMonth() + 1)).substr(-2)}-${('0' + date.getUTCDate()).substr(-2)}`;
-
-				var tileLayer = L.tileLayer(`http://{s}.sat.owm.io/sql/{z}/{x}/{y}?op=truecolor&from=${type}&where=day:${day}&appid=${appId}&overzoom=true`, {
+                const url = `http://{s}.sat.owm.io/sql/{z}/{x}/{y}?op=truecolor&from=${type}&where=day:${day}&appid=${appId}&overzoom=true`;
+				var tileLayer = L.tileLayer(url, {
 					attribution: '&copy; <a href="http://owm.io">VANE</a>',
 					maxZoom: 33
 				}).addTo(map);
+
+
+				map.on('click', function(e) {
+					var latlng = e.latlng.wrap();
+					var zxy = getCoords(this, latlng);
+
+					L.popup()
+						.setLatLng(latlng)
+						.setContent('<p>lat: ' + latlng.lat + '<br />lon: ' + latlng.lng + `<br />tile: <a target="new" href="${url.replace('{s}', 'a').replace('/{z}/{x}/{y}', zxy)}">` + zxy + '</a></p>')
+						.openOn(map);
+				});
 			} catch (err) {
 				alert(err.message);
 			}
